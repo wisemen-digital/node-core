ee4b093cfc361b348903d06097865d9657a92aa1
Kobe Kwanten <kobe.kwanten@appwise.be>
2023-06-14 16:53:23 +0200

    Add overlap validator

--- packages/planning/lib/tests/planning-event.sample.ts
+++ packages/planning/lib/tests/planning-event.sample.ts
@@ -1,13 +1,17 @@
 import {PlanningEvent} from '../planning-event.js'
 import moment from 'moment'
 import { YYYY_MM_DD } from '../constants.js'
+import {defaultToIfUndefined} from "../util/default-to-if-undefined.js";
+import {Time} from "@appwise/time";
 
 export function createPlanningEvent(withValues?: Partial<PlanningEvent>): PlanningEvent {
   return {
     isRecurring: false,
-    startDate: moment().format(YYYY_MM_DD),
-    endDate: moment().format(YYYY_MM_DD),
-    weeksPeriod: null,
-    ...withValues
+    startDate: defaultToIfUndefined(withValues?.startDate, moment().format(YYYY_MM_DD)),
+    endDate: defaultToIfUndefined(withValues?.endDate, moment().format(YYYY_MM_DD)),
+    weeksPeriod: defaultToIfUndefined(withValues?.weeksPeriod, null),
+    exceptions: defaultToIfUndefined(withValues?.exceptions, []),
+    startTime: defaultToIfUndefined(withValues?.startTime, Time.fromString('09:00:00')),
+    endTime: defaultToIfUndefined(withValues?.endTime, Time.fromString('10:00:00'))
   }
 }
--- packages/planning/lib/tests/recurring-event.sample.ts
+++ packages/planning/lib/tests/recurring-event.sample.ts
@@ -1,13 +1,19 @@
 import moment from "moment"
 import { YYYY_MM_DD } from '../constants.js'
 import { RecurringEvent } from '../recurring-event.js'
+import {Time} from "@appwise/time";
+import {defaultToIfUndefined} from "../util/default-to-if-undefined.js";
 
 export function createRecurringEvent(withValues?: Partial<RecurringEvent>): RecurringEvent {
   return {
     isRecurring: true,
-    startDate: moment().format(YYYY_MM_DD),
-    endDate: moment().add(1,'week').format(YYYY_MM_DD),
-    weeksPeriod: 1,
-    ...withValues
+    startDate: defaultToIfUndefined(withValues?.startDate, moment().format(YYYY_MM_DD)),
+    endDate: defaultToIfUndefined(withValues?.endDate, moment().add(1,'week').format(YYYY_MM_DD)),
+    weeksPeriod: defaultToIfUndefined(withValues?.weeksPeriod, 1),
+    exceptions: defaultToIfUndefined(withValues?.exceptions, []),
+    startTime: defaultToIfUndefined(withValues?.startTime, Time.fromString('09:00:00')),
+    endTime: defaultToIfUndefined(withValues?.endTime, Time.fromString('10:00:00'))
   }
 }
+
+
