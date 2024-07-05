462c415ccf72a408c1aa3583d1aa0eeff5ab71e3
Maarten Sijmkens <maarten.sijmkens@gmail.com>
2024-07-05 14:21:51 +0200

    basic implementation

---
+++ packages/date-time-range/lib/tests/date-time-range.unit.test.ts
@@ -0,0 +1,88 @@
+import { describe, it } from 'node:test'
+import { expect } from 'expect'
+import dayjs from 'dayjs'
+import { DateTimeRange } from '../date-time-range.js'
+import exp from 'node:constants'
+
+describe('DateTimeRange class', () => {
+  describe('constructor', () => {
+    it('Throw error when upperbound is after lowerbound', () => {
+      const lowerBound = dayjs('2024-01-01 17:00:00')
+      const upperBound = dayjs('2024-01-01 08:00:00')
+
+      expect(() => new DateTimeRange(lowerBound, upperBound, '[]')).toThrow()
+    })
+  })
+
+  const lowerBound = dayjs('2024-01-01 08:00:00')
+  const upperBound = dayjs('2024-01-01 17:00:00')
+  const dateBetweenBounds = dayjs('2024-01-01 12:00:00')
+  const dateBeforeLowerBound = dayjs('2024-01-01 07:59:59')
+  const dateAfterLowerBound = dayjs('2024-01-01 17:00:01')
+
+  describe('contains', () => {
+    it('Closed range []', () => {
+      const range = new DateTimeRange(lowerBound, upperBound, '[]')
+
+      expect(range.contains(lowerBound)).toBeTruthy()
+      expect(range.contains(upperBound)).toBeTruthy()
+      expect(range.contains(dateBetweenBounds)).toBeTruthy()
+      expect(range.contains(dateBeforeLowerBound)).toBeFalsy()
+      expect(range.contains(dateAfterLowerBound)).toBeFalsy()
+    })
+
+    it('Open range ()', () => {
+      const range = new DateTimeRange(lowerBound, upperBound, '()')
+
+      expect(range.contains(lowerBound)).toBeFalsy()
+      expect(range.contains(upperBound)).toBeFalsy()
+      expect(range.contains(dateBetweenBounds)).toBeTruthy()
+      expect(range.contains(dateBeforeLowerBound)).toBeFalsy()
+      expect(range.contains(dateAfterLowerBound)).toBeFalsy()
+    })
+
+    it('Closed-Open range [)', () => {
+      const range = new DateTimeRange(lowerBound, upperBound, '[)')
+
+      expect(range.contains(lowerBound)).toBeTruthy()
+      expect(range.contains(upperBound)).toBeFalsy()
+      expect(range.contains(dateBetweenBounds)).toBeTruthy()
+      expect(range.contains(dateBeforeLowerBound)).toBeFalsy()
+      expect(range.contains(dateAfterLowerBound)).toBeFalsy()
+    })
+
+    it('Open-Closed range ()', () => {
+      const range = new DateTimeRange(lowerBound, upperBound, '(]')
+
+      expect(range.contains(lowerBound)).toBeFalsy()
+      expect(range.contains(upperBound)).toBeTruthy()
+      expect(range.contains(dateBetweenBounds)).toBeTruthy()
+      expect(range.contains(dateBeforeLowerBound)).toBeFalsy()
+      expect(range.contains(dateAfterLowerBound)).toBeFalsy()
+    })
+  })
+
+
+  describe('contains', () => {
+    it('infinity upperbound', () => {
+      const range = new DateTimeRange(lowerBound, null, '[]')
+
+      expect(range.contains(lowerBound)).toBeTruthy()
+      expect(range.contains(upperBound)).toBeTruthy()
+      expect(range.contains(dateBetweenBounds)).toBeTruthy()
+      expect(range.contains(dateBeforeLowerBound)).toBeFalsy()
+      expect(range.contains(dateAfterLowerBound)).toBeTruthy()
+    })
+
+    it('infinity lowerbound', () => {
+      const range = new DateTimeRange(null, upperBound, '[]')
+
+      expect(range.contains(lowerBound)).toBeTruthy()
+      expect(range.contains(upperBound)).toBeTruthy()
+      expect(range.contains(dateBetweenBounds)).toBeTruthy()
+      expect(range.contains(dateBeforeLowerBound)).toBeTruthy()
+      expect(range.contains(dateAfterLowerBound)).toBeFalsy()
+    })
+  })
+})
+
