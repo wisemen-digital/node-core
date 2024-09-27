c016f071c3ee37db06ac445906e9b1ece8986b43
Joren Vandeweyer <joren.vandeweyer@appwise.be>
2024-09-27 10:51:01 +0200

    remove init

--- packages/app-container/lib/containers/default.ts
+++ packages/app-container/lib/containers/default.ts
@@ -36,8 +36,6 @@ export abstract class ProbedContainer {
 
       this.nest = await this.bootstrap(adapter)
 
-      await this.nest.init()
-
       this.state = 'ready'
     } catch (e) {
       await this.close()
