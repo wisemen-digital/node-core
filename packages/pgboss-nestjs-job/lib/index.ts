2e9b8c034eeedd5b922d8677ad9e61a652c95725
Pieter <pieter.pauwels@appwise.be>
2025-01-20 16:46:34 +0100

    Export entities and scheduling types

--- packages/pgboss-nestjs-job/lib/index.ts
+++ packages/pgboss-nestjs-job/lib/index.ts
@@ -1,3 +1,8 @@
+// Entities
+export { Job } from './persistence/job.entity.js'
+export { ArchivedJob } from './persistence/archive.entity.js'
+
+// Defining jobs
 export { PgBossWorkerModule } from './worker/pgboss-worker.module.js'
 
 export { PgBossJob } from './jobs/job.decorator.js'
@@ -6,3 +11,7 @@ export { PgBossJobHandler } from './jobs/job.decorator.js'
 export { BaseJobConfig } from './jobs/job.abstract.js'
 export { BaseJobData } from './jobs/job.abstract.js'
 export { BaseJobHandler } from './jobs/job.abstract.js'
+
+// Scheduling jobs
+export { PgBossSchedulerModule } from './scheduler/pgboss-scheduler.module.js'
+export { PgBossScheduler } from './scheduler/pgboss-scheduler.service.js'
