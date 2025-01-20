e1e55b27d3d35de04aee0991c67e90bef8282926
Pieter <pieter.pauwels@appwise.be>
2025-01-20 16:35:37 +0100

    Move to lib folder and create index.ts file

---
+++ packages/pgboss-nestjs-job/lib/index.ts
@@ -0,0 +1,8 @@
+export { PgBossWorkerModule } from './worker/pgboss-worker.module.js'
+
+export { PgBossJob } from './jobs/job.decorator.js'
+export { PgBossJobHandler } from './jobs/job.decorator.js'
+
+export { BaseJobConfig } from './jobs/job.abstract.js'
+export { BaseJobData } from './jobs/job.abstract.js'
+export { BaseJobHandler } from './jobs/job.abstract.js'
