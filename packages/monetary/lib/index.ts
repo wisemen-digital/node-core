a098d8f7c59c57f19f120b00b2653373ad58d121
Kobe Kwanten <kobe.kwanten@appwise.be>
2025-01-23 15:18:07 +0100

    Singleton monetary transformer

--- packages/monetary/lib/index.ts
+++ packages/monetary/lib/index.ts
@@ -1,4 +1,4 @@
 export * from './monetary.js'
 export * from './monetary.dto.js'
-export * from './monetary.column.js'
+export { MonetaryColumn } from './monetary.column.js'
 export * from './monetary.object.js'
