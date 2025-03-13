56f07f11a4b44f50a38dae90c91749870784ee39
Kobe Kwanten <kobe.kwanten@appwise.be>
2025-03-13 21:18:08 +0100

    feat: update paginated keyset dependencies

--- packages/pagination/lib/keyset/paginated-keyset.query.ts
+++ packages/pagination/lib/keyset/paginated-keyset.query.ts
@@ -2,7 +2,7 @@ import { Type } from 'class-transformer'
 import { IsInt, IsPositive, Max } from 'class-validator'
 import { ApiProperty } from '@nestjs/swagger'
 import { IsUndefinable } from '@wisemen/validators'
-import { SearchQuery } from '../../query/search.query.js'
+import { SearchQuery } from '../search.query.js'
 
 export abstract class PaginatedKeysetQuery {
   @ApiProperty({ maximum: 100, minimum: 0 })
