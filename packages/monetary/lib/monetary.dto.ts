9c790ab6163e686305dd77b23c4f31db134037ab
Senne Van Reusel <senne.vanreusel@wisemen.digital>
2025-04-02 15:00:58 +0200

    added extra cases

--- packages/monetary/lib/monetary.dto.ts
+++ packages/monetary/lib/monetary.dto.ts
@@ -19,6 +19,10 @@ export class MonetaryDto {
   static from (monetary: undefined): undefined
   static from (monetary: null): null
   static from (monetary: Monetary): MonetaryDto
+  static from (monetary: undefined | null): undefined | null
+  static from (monetary: Monetary | null): MonetaryDto | null
+  static from (monetary: Monetary | undefined): MonetaryDto | undefined
+  static from (monetary: Monetary | null | undefined): MonetaryDto | null | undefined
   static from (monetary: Monetary | null | undefined): MonetaryDto | null | undefined {
     if (monetary === null) return null
     if (monetary === undefined) return undefined
