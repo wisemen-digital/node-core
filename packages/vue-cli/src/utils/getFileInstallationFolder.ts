a6d4a46450b287b87c3bcdd8f66306102230e58c
Joren Vandeweyer <joren.vandeweyer@appwise.be>
2025-04-11 10:52:46 +0200

    updated packages

--- packages/vue-cli/src/utils/getFileInstallationFolder.ts
+++
@@ -1,6 +0,0 @@
-import { FileType } from "./getComponents";
-import { Config } from "./getConfig";
-
-export const getFileInstallationFolder = (fileType: FileType, config: Config) => {
-  return config.resolvedPaths[fileType]
-}
--- packages/vue-cli/src/utils/staticVariables.ts
+++
@@ -1,1 +0,0 @@
-export const baseUrl = process.env.COMPONENTS_BASE_URL ?? "https://wisemen-digital.github.io/vue-components"
